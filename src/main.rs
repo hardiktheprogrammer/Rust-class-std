02a0706572b9d2bc135adc13054160e3bc8e492b
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-03-10 04:22:03 +0530

    add function , Datatype files

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-
+fn main() {
+    let x= 10;
+    println!("this is the value of x: {}" , x);
+    let x = "six";
+    println!("this is the right value of x: {}", x);
+    // x = 10;
+    const HardikFlowers: u32 = 1000_000;
+}
